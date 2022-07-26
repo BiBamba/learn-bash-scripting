75b59a530aa2b7083615f6dab1cc4d58107c9a26
Bi Bamba <maxime-tizie@hotmail.fr>
2022-07-26 12:43:23 -0500

    Script on definition of variables

---
+++ variables/myvariables.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This a comment Line
+
+# System defined variables
+echo My shell name is $BASH
+echo My bash version is $BASH_VERSION
+echo My Home directory $HOME
+echo My current working directory is $PWD
+echo My path is $PATH
+
+# Users defined variables
+name=Bi
+echo My name is $name
